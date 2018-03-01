7b62002f75ac64f32937fae04a502b2fd80c9f39
KYASURlN <33185096+KYASURlN@users.noreply.github.com>
2018-02-28 19:41:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const args = message.content.trim().split(/ +/g);
+const command = args.shift().toLowerCase();
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('pong');
-  	}
-    if (message.content === 'tomo is gay') {
-    	message.channel.send('A-Ah! Đúng rồi!');
-  	}
+    if(command === 'ping') {
+      message.channel.send('Pong!');
+      } else
+    if (command === 'blah') {
+      message.channel.send('Meh.');
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
